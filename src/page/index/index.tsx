b4b0a12ad2419cec9dd7cdddb274a5fd6e7fd577
artem <artem.v@zimalab.com>
2020-02-21 09:54:54 +0700

    using_styled_component_with_ts

--- src/page/index/index.tsx
+++ src/page/index/index.tsx
@@ -1,11 +1,34 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+import styled from 'styled-components';
 
 import { AllCryptoCoinTable} from "../../components/index/all-crypto-coin-table";
 
+
+import {AppState} from "../../store";
+
+import {Section, Paragraph, ButtonSuccess} from '../../ui/index';
+
 export const Home = () => {
+
+	const isLoading = useSelector(
+		(state: AppState) => state.CryptoReducer.isLoading
+	)
+
+	let primary = isLoading ? 'primary' : ''
 
 	return (
 		<div>
+				<Section>
+					<Paragraph>
+						Look at my buttons, they are amazing buttons !
+					</Paragraph>
+					<ButtonSuccess primary={primary}>
+						Click
+					</ButtonSuccess>
+				</Section>
+
 			<section className="home">
 				<AllCryptoCoinTable />
 			</section>
